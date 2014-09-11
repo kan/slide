c8406058fea916592e74a24b5a9398dd3683bc0a
Kan Fushihara <kan.fushihara@gmail.com>
2014-09-11 13:47:07 +0900

    first commit

---
+++ slide.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "os"
+	"fmt"
+	"net/http"
+    "io/ioutil"
+)
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+    http.HandleFunc("/markdown", mdHandler)
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+	panic(http.ListenAndServe(":17901", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    src, err := Asset("static/lite.html")
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Fprintf(w, string(src))
+    }
+}
+
+func mdHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("open %s\n", os.Args[1])
+    contents, err := ioutil.ReadFile(os.Args[1])
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Fprintf(w, string(contents))
+    }
+}
